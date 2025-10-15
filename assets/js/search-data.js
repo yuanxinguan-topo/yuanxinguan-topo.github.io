4577a764d0a92eda1bad2a1fab5c464a3c142a7e
yuanxinguan-topo <yxguan21@m.fudan.edu.cn>
2025-10-15 10:44:32 +0000

    Deploying to gh-pages from @ yuanxinguan-topo/yuanxinguan-topo.github.io@2a9bb68b077ea6e191b8d9c1766009ae716bbfd2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-home",
+    title: "Home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
